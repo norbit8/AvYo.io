8f4af77b65f604525dece75dae4b47f66cf53028
YoavL <norbit8@gmail.com>
2018-01-08 18:53:34 +0200

    Added stars
    function stars() adds stars which will give points if a player gets them.

---
+++ public/Star.js
@@ -0,0 +1,13 @@
+class Star() {
+  constructor(wd, he) {
+    this.w = w;
+    this.h = h;
+    this.duration = 10;
+  }
+
+  show() {
+    fill(255, 204, 0);
+    strokeWeight(4);
+    point(this.w, this.h);
+  }
+}
